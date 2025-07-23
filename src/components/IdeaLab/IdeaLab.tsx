b284d54b450e0a0e4404bf3f7256af322257ea6e
uneedcomms-luca <luca.im@uneedcomms.com>
2025-07-23 16:00:17 +0900

    feat : 채팅 메시지 추가

--- src/components/IdeaLab/IdeaLab.tsx
+++ src/components/IdeaLab/IdeaLab.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import ChattingInput from "./ChattingInput";
+import ChattingContent from "./ChattingContent";
+import { Message } from "@/types/chatting";
 
 export default function IdeaLab() {
   const [inputValue, setInputValue] = useState("");
@@ -9,18 +11,21 @@ export default function IdeaLab() {
     console.log("submit");
   };
 
+  const [messages, setMessages] = useState<Message[]>([
+    new Message("안녕하세요", "user"),
+    new Message("안녕하세요", "assistant")
+  ]);
+
   return (
     <div className="content-container content-container-right lg:flex-1 h-full flex flex-col">
       <div className="content-container-header">
         <h1>Idea Lab</h1>
       </div>
-      <div className="flex-1 p-4 bg-red-50 overflow-y-auto">HI</div>
+      <div className="flex-1 p-4 bg-white overflow-y-auto">
+        <ChattingContent messages={messages} isLoading={false} />
+      </div>
 
-      <ChattingInput
-        inputValue={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-        onSubmit={handleSubmit}
-      />
+      <ChattingInput inputValue={inputValue} onChange={(e) => setInputValue(e.target.value)} onSubmit={handleSubmit} />
     </div>
   );
 }
